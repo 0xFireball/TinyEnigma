b9100b62d0a5eb764492d504ccefac46b48b0235
0xFireball <0xFireball@outlook.com>
2016-05-31 18:16:06 -0700

    set up xammac project

---
+++ TinyEnigma/TinyEnigma.XMac/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace TinyEnigma.XMac
+{
+	static class MainClass
+	{
+		static void Main (string [] args)
+		{
+			NSApplication.Init ();
+			NSApplication.Main (args);
+		}
+	}
+}
