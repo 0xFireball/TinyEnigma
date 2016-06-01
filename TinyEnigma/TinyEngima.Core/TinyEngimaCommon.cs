e9f2d411e2428146a94f2545c5f6721e64bbad67
0xFireball <0xFireball@outlook.com>
2016-05-31 17:57:58 -0700

    add all the utils

---
+++ TinyEnigma/TinyEngima.Core/TinyEngimaCommon.cs
@@ -0,0 +1,22 @@
+﻿/*
+ */
+using System;
+using System.IO;
+
+namespace TinyEngima.Core
+{
+	/// <summary>
+	/// Common class for TinyEnigma
+	/// </summary>
+	public class TinyEngimaCommon
+	{
+		public TinyEnigmaCryptor Cryptor = new TinyEnigmaCryptor();
+		
+		public void RunCryptoOnFile(string inputFile, string outputFile, string passphrase, bool encrypt)
+		{
+			byte[] inputBytes = File.ReadAllBytes(inputFile);
+			byte[] outputBytes = encrypt ? Cryptor.GetEncryptedBytes(inputBytes, passphrase) : Cryptor.GetDecryptedBytes(inputBytes, passphrase);
+			File.WriteAllBytes(outputFile, outputBytes);
+		}
+	}
+}
---
+++ TinyEnigma/TinyEngima.Core/TinyEnigmaCryptor.cs
@@ -0,0 +1,27 @@
+﻿/*
+ */
+using System;
+using OmniBean.PowerCrypt4;
+using OmniBean.PowerCrypt4.Utilities;
+
+namespace TinyEngima.Core
+{
+	public class TinyEnigmaCryptor
+	{
+		private readonly AESProvider _aesProvider;
+		public TinyEnigmaCryptor()
+		{
+			_aesProvider = new AESProvider();
+		}
+		
+		public byte[] GetEncryptedBytes(byte[] input, string passphrase)
+		{
+			return _aesProvider.EncryptString(input.GetString(), passphrase).GetBytes();
+		}
+		
+		public byte[] GetDecryptedBytes(byte[] input, string passphrase)
+		{
+			return _aesProvider.DecryptString(input.GetString(), passphrase).GetBytes();
+		}
+	}
+}
