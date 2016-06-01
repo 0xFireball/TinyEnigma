887b7504fc7758a7ebbd449225075cead8c9db2e
0xFireball <0xFireball@outlook.com>
2016-05-31 17:51:11 -0700

    design a super basic ui

---
+++ TinyEnigma/TinyEngima.WinForms/MainForm.Designer.cs
@@ -0,0 +1,112 @@
+﻿/*
+ */
+namespace TinyEngima.WinForms
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(131, 46);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(139, 32);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "TinyEnigma";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(49, 117);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(210, 19);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Select a file to encrypt or decrypt";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(49, 139);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 28);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "Browse";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(235, 238);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 31);
+			this.button2.TabIndex = 3;
+			this.button2.Text = "Encrypt";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(316, 238);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 31);
+			this.button3.TabIndex = 4;
+			this.button3.Text = "Decrypt";
+			this.button3.UseVisualStyleBackColor = true;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(403, 281);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "MainForm";
+			this.Text = "TinyEngima";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
